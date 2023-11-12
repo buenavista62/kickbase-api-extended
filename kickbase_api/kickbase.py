4cb50ff596e7e88cde4425852f52213fcb0887de
Sercan Ates <sercan-ates@outlook.com>
2023-11-12 04:04:11 +0100

    modified

--- kickbase_api/kickbase.py
+++ kickbase_api/kickbase.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta, timezone
 from typing import Union
-
 import requests
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
 
 from kickbase_api.exceptions import KickbaseLoginException, KickbaseException
 from kickbase_api.models._transforms import parse_date, date_to_string
@@ -23,7 +24,6 @@ from kickbase_api.models.player import Player
 from kickbase_api.models.response.league_stats_response import LeagueStatsResponse
 from kickbase_api.models.user import User
 
-
 class Kickbase:
     base_url: str = None
     token: str = None
@@ -40,7 +40,9 @@ class Kickbase:
         self.base_url = base_url
         self.firestore_project = firestore_project
         self.google_identity_toolkit_api_key = google_identity_toolkit_api_key
-
+        # Hinzufügen einer wiederverwendbaren Session:
+        self.session = requests.Session()
+        
     def login(self, username: str, password: str) -> (User, [LeagueData]):
         data = {
             "email": username,
@@ -234,7 +236,7 @@ class Kickbase:
             raise KickbaseException()
 
     def search_player(self, search_query: str) -> [Player]:
-        r = self._do_get("/competition/search?t={}".format(search_query))
+        r = self._do_get("/competition/search?t={}".format(search_query),True)
 
         if r.status_code == 200:
             return [Player(v) for v in r.json()["p"]]
@@ -242,12 +244,72 @@ class Kickbase:
             raise KickbaseException()
 
     def team_players(self, team_id: str) -> [Player]:
-        r = self._do_get("/competition/teams/{}/players".format(team_id))
+        r = self._do_get("/competition/teams/{}/players".format(team_id),True)
 
         if r.status_code == 200:
             return [Player(v) for v in r.json()["p"]]
         else:
             raise KickbaseException()
+
+    """    
+    def fetch_players_in_range(self, start, end):
+        url = f"/competition/search?start={start}&end={end}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            return [Player(v) for v in r.json()["p"]]
+        else:
+            raise KickbaseException()
+
+    def fetch_user_id(self, player, league_id):
+        url = f"/leagues/{league_id}/players/{player.id}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            user_id = r.json().get("userId")
+            if user_id is not None:
+                player.user_id = user_id
+        return player
+
+    def get_all_players(self, league_id) -> [Player]:
+        players = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            ranges = [(i, i + 24) for i in range(0, 700, 25)]
+            player_lists = executor.map(lambda r: self.fetch_players_in_range(*r), ranges)
+            players = [player for player_list in player_lists for player in player_list]
+        
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            players = list(executor.map(lambda p: self.fetch_user_id(p, league_id), players))
+
+        return players """
+    
+    def get_player_market_value_last_n_days(self, league_id: str, player_id: str, days: int = 30) -> dict:
+        """Get the market value of a player from the last n days.
+
+        Parameters:
+        - league_id (str): The ID of the league.
+        - player_id (str): The ID of the player.
+        - days (int): Number of days to retrieve the market value for (default is 30 days, max is 360 days).
+
+        Returns:
+        - dict: Dictionary containing the market values for the last n days.
+        """
+        if days > 360:
+            raise ValueError("The maximum number of days is 360.")
+        
+        endpoint = f"/leagues/{league_id}/players/{player_id}/stats"
+        response = self._do_get(endpoint, True)
+        data = response.json()
+
+        # Extract the marketValues list from the data
+        market_values_list = data["marketValues"]
+
+        # Sort the marketValues list by date in descending order
+        sorted_data = sorted(market_values_list, key=lambda x: datetime.fromisoformat(x['d'].replace("Z", "")), reverse=True)
+        
+        # Get the market values for the last n days
+        last_n_days_market_values = [x['m'] for x in sorted_data[:days]]
+        last_n_days_market_values.reverse()
+        return last_n_days_market_values
+
 
     def top_25_players(self) -> [Player]:
         r = self._do_get("/competition/best?position=0")
@@ -551,10 +613,7 @@ class Kickbase:
     def _auth_cookie(self):
         return "kkstrauth={}".format(self.token)
 
-    def _do_get(self, endpoint: str, authenticated: bool = False):
-        if authenticated and  not self._is_token_valid():
-            self.login(self._username, self._password)
-
+    def _do_get(self, endpoint: str, authenticated: bool = False, retries=3):
         headers = {
             "Content-Type": "application/json",
             "Accept": "application/json"
@@ -562,8 +621,18 @@ class Kickbase:
         if authenticated:
             headers["Cookie"] = self._auth_cookie()
 
-        return requests.get(self._url_for_endpoint(endpoint), headers=headers)
-
+        for i in range(retries):
+            try:
+                r = self.session.get(self._url_for_endpoint(endpoint), headers=headers)
+                r.raise_for_status()
+                return r
+            except requests.RequestException as e:
+                if i < retries - 1:
+                    time.sleep(0.5)
+                    continue
+                else:
+                    raise
+                
     def _do_post(self, endpoint: str, data: dict, authenticated: bool = False):
         if authenticated and not self._is_token_valid():
             self.login(self._username, self._password)
@@ -602,3 +671,61 @@ class Kickbase:
             headers["Cookie"] = self._auth_cookie()
 
         return requests.delete(self._url_for_endpoint(endpoint), headers=headers)
+
+    def fetch_players_in_range(self, start, end):
+        url = f"/competition/search?start={start}&end={end}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            return [Player(v) for v in r.json()["p"]]
+        else:
+            raise KickbaseException()
+
+    def fetch_user_id(self, player, league_id):
+        url = f"/leagues/{league_id}/players/{player.id}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            user_id = r.json().get("userId")
+            if user_id is not None:
+                player.user_id = user_id
+        return player
+
+    def get_all_players(self, league_id) -> [Player]:
+        players = []
+        ranges = [(i, i + 24) for i in range(0, 600, 25)]
+
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            futures = [executor.submit(self.fetch_players_in_range, *r) for r in ranges]
+            for future in as_completed(futures):
+                try:
+                    players.extend(future.result())
+                except Exception as e:
+                    print(f"Error fetching players in range: {e}")
+
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            futures = [executor.submit(self.fetch_user_id, p, league_id) for p in players]
+            for i, future in enumerate(as_completed(futures)):
+                try:
+                    players[i] = future.result()
+                except Exception as e:
+                    print(f"Error fetching user id for player: {e}")
+
+        return players
+
+    def leagueTable(self, matchDay = 0):
+        url = f"/competition/table?matchDay={matchDay}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            data = r.json().get("t")
+            return data
+        else:
+            raise KickbaseException()
+    
+    def matches(self, matchDay = 0):
+        url = f"/competition/matches?matchDay={matchDay}"
+        r = self._do_get(url, True)
+        if r.status_code == 200:
+            data = r.json().get("m")
+            return data
+        else:
+            raise KickbaseException()
+            
